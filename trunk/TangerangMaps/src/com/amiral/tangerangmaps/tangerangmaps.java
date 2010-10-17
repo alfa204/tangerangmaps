51b3672c607a251c25a6279a141e7d8993237cbf
4miral <4miral@20f5fc73-cbb3-d42b-e17d-327d00e7f6fa>
2010-10-17 15:54:30 +0000

    COmmit dulu dah, untuk modelnya. masih berantakkan

---
+++ trunk/TangerangMaps/src/com/amiral/tangerangmaps/tangerangmaps.java
@@ -0,0 +1,60 @@
+package com.amiral.tangerangmaps;
+
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+public class tangerangmaps extends TabActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //Menginisalisasi TAB
+        TabHost host= (TabHost) findViewById(R.id.tabs);
+        TabHost.TabSpec tabSpec;
+        Intent intent; //yang digunakan untuk memanggil tab lainnya
+        
+        intent = new Intent().setClass(this, map.class); //untuk inisialisasi tab pertama kali di jalankan
+        tabSpec = host.newTabSpec("maptangerang")
+        		.setIndicator(
+        				new Tampilan(this, R.drawable.icon, R.string.tab_map))
+        				.setContent(intent);
+        host.addTab(tabSpec);
+        
+        //tab Augmented Reality
+        
+        tabSpec= host.newTabSpec("AugmentedReality")
+        	.setIndicator(
+        		new Tampilan(this, R.drawable.icon, R.string.tab_ar))
+        		.setContent(intent);
+        host.addTab(tabSpec);
+    }
+    
+    private class Tampilan extends LinearLayout {
+
+		public Tampilan(Context context, int gambar, int label) {
+			super(context);
+			// TODO Auto-generated constructor stub
+
+			ImageView iv = new ImageView(context);
+			TextView tv = new TextView(context);
+
+			iv.setImageResource(gambar);
+			tv.setText(label);
+			tv.setGravity(0x01); // posisi ditengah
+
+			setOrientation(LinearLayout.VERTICAL);
+			addView(iv);
+			addView(tv);
+
+		}
+
+	}
+}
