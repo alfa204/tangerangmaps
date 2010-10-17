4ba23a6e86ded0fba71ef2b12d671bbcd02e61b1
4miral <4miral@20f5fc73-cbb3-d42b-e17d-327d00e7f6fa>
2010-10-17 15:54:30 +0000

    COmmit dulu dah, untuk modelnya. masih berantakkan

---
+++ TangerangMaps/src/com/amiral/tangerangmaps/map.java
@@ -0,0 +1,16 @@
+package com.amiral.tangerangmaps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+
+public class map extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+	}
+	
+
+}
