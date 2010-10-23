b1219e4598b60b2fd4aebb2915ee76f79f14794c
4miral <4miral@20f5fc73-cbb3-d42b-e17d-327d00e7f6fa>
2010-10-23 07:07:20 +0000

    UI sudah mulai fix, Layar sudah OK

--- trunk/TangerangMaps/src/com/amiral/tangerangmaps/AddPOI.java
+++ trunk/TangerangMaps/src/com/amiral/tangerangmaps/AddPOI.java
@@ -2,6 +2,10 @@ package com.amiral.tangerangmaps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddPOI extends Activity {
 
@@ -9,5 +13,21 @@ public class AddPOI extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addlocation);
+		
+		 Spinner kategori = (Spinner) findViewById(R.id.kategori);
+		    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+		            this, R.array.list_kategori, android.R.layout.simple_spinner_item);
+		    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		    kategori.setAdapter(adapter);
 	}
+	
+	public void lihatPeta(View tombol){
+		Toast.makeText(getBaseContext(), "Diklik tampil PETA", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void tambahLokasi(View tombol){
+		Toast.makeText(getBaseContext(), "Tanda berhasil disimpan", Toast.LENGTH_SHORT).show();
+	}
+	
+	
 }
