f2045e8ad462af455e4fc19a05cd4cd1e3d1e69d
4miral <4miral@20f5fc73-cbb3-d42b-e17d-327d00e7f6fa>
2010-11-09 03:08:57 +0000

    Konfigurasi tampilan Dashboard

--- TangerangMaps/src/com/amiral/tangerangmaps/AddPOI.java
+++ TangerangMaps/src/com/amiral/tangerangmaps/AddPOI.java
@@ -4,15 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddPOI extends Activity {
 
+	private TextView lon;
+	private TextView lat;
+	private Button cekPeta;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addlocation);
+		
+		lat = (TextView) findViewById(R.id.label_latitude);
+		lon = (TextView) findViewById(R.id.label_longitude);
 		
 		 Spinner kategori = (Spinner) findViewById(R.id.kategori);
 		    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -23,6 +31,8 @@ public class AddPOI extends Activity {
 	
 	public void lihatPeta(View tombol){
 		Toast.makeText(getBaseContext(), "Diklik tampil PETA", Toast.LENGTH_SHORT).show();
+		lat.setText("Latitude dari Peta");
+		lon.setText("longitude dari Peta");
 	}
 	
 	public void tambahLokasi(View tombol){
